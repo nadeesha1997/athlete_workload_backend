c656eef701331c6ce53cee4610e0641a29780522
nadeesha1997 <nadeeshamadhushan9@gmail.com>
2022-07-20 16:55:19 +0530

    unsuccess

--- predict/views.py
+++ predict/views.py
@@ -8,7 +8,7 @@ from rest_framework import status
 from mlmodel.models import MlModel, ModelSport
 
 from predict.serializers import PredictSerializer, WorkloadSerializer
-from services.prediction import predict
+# from services.prediction import predict
 from datetime import datetime
 
 # Create your views here.
@@ -22,36 +22,36 @@ class PredictView(generics.GenericAPIView):
     queryset = Predict.objects.all()
     permission_classes = [IsAuthenticated]
 
-    def post(self, request, date):
-        all_data = MergeData.objects.all().filter(user_id=request.user.id, date=date)[0]
-        test_data = all_data.data["har"]
-        sport = SportUser.objects.filter(user_id=request.user.id)[0].sport
-        ml_model = ModelSport.objects.filter(sport=sport)[0].first_model
-        # try:
-        output = predict(ml_model.model_file.path, test_data)
-        activities = Activity.objects.filter(sport=sport)
-        activity_count = {}
-        for activity in activities:
-            activity_count[activity.id_field] = 0
-        for val in output:
-            activity_count[val] = activity_count[val] + 1
-        heart_rate = all_data.data["hr"]
-        average_heart_rate = 0
-        if len(heart_rate) > 0:
-            average_heart_rate = sum(heart_rate) / len(heart_rate)
-        user = all_data.user
-        model_date = all_data.date
-        wl_data={"har":activity_count,"hr":average_heart_rate}
-        final_data = {
-            "workload_data": wl_data,
-            "date": model_date,
-            "user": user.id
-        }
-        workload_serializer = WorkloadSerializer(data=final_data)
-        if workload_serializer.is_valid(raise_exception=True):
-            workload_serializer.save()
-            return Response(workload_serializer.data, status=status.HTTP_200_OK)
-        return Response(workload_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def post(self, request, date):
+    #     all_data = MergeData.objects.all().filter(user_id=request.user.id, date=date)[0]
+    #     test_data = all_data.data["har"]
+    #     sport = SportUser.objects.filter(user_id=request.user.id)[0].sport
+    #     ml_model = ModelSport.objects.filter(sport=sport)[0].first_model
+    #     # try:
+    #     output = predict(ml_model.model_file.path, test_data)
+    #     activities = Activity.objects.filter(sport=sport)
+    #     activity_count = {}
+    #     for activity in activities:
+    #         activity_count[activity.id_field] = 0
+    #     for val in output:
+    #         activity_count[val] = activity_count[val] + 1
+    #     heart_rate = all_data.data["hr"]
+    #     average_heart_rate = 0
+    #     if len(heart_rate) > 0:
+    #         average_heart_rate = sum(heart_rate) / len(heart_rate)
+    #     user = all_data.user
+    #     model_date = all_data.date
+    #     wl_data={"har":activity_count,"hr":average_heart_rate}
+    #     final_data = {
+    #         "workload_data": wl_data,
+    #         "date": model_date,
+    #         "user": user.id
+    #     }
+    #     workload_serializer = WorkloadSerializer(data=final_data)
+    #     if workload_serializer.is_valid(raise_exception=True):
+    #         workload_serializer.save()
+    #         return Response(workload_serializer.data, status=status.HTTP_200_OK)
+    #     return Response(workload_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     def get(self,request,date):
         user=request.user
         workload_data = Workload.objects.filter(user_id=request.user.id)
--- services/prediction.py
+++ services/prediction.py
@@ -1,30 +1,30 @@
 import numpy as np
 from sklearn.preprocessing import LabelEncoder, StandardScaler
-from tensorflow import keras
-import tensorflow as tf
+# from tensorflow import keras
+# import tensorflow as tf
 import pandas as pd
 
 frame_size = 100
 hop_size = 1
 
 
-def predict(model, data):
-    data_formatted = []
-    for key in data:
-        data_formatted.append(data[key])
-    x = pd.DataFrame(data_formatted)
-    predictions = []
-    ml_model = tf.keras.models.load_model(model)
-    scaler = StandardScaler()
-    # print(x)
-    x_new = scaler.fit_transform(x)
-    s_x = pd.DataFrame(data=x_new)
-    x_frames = get_frames(s_x, frame_size, hop_size)
-    temp_data = ml_model.predict(x_frames)
-    y_pred = np.argmax(temp_data, axis=1)
-    for x in y_pred:
-        predictions.append(x)
-    return predictions
+# def predict(model, data):
+#     data_formatted = []
+#     for key in data:
+#         data_formatted.append(data[key])
+#     x = pd.DataFrame(data_formatted)
+#     predictions = []
+#     ml_model = tf.keras.models.load_model(model)
+#     scaler = StandardScaler()
+#     # print(x)
+#     x_new = scaler.fit_transform(x)
+#     s_x = pd.DataFrame(data=x_new)
+#     x_frames = get_frames(s_x, frame_size, hop_size)
+#     temp_data = ml_model.predict(x_frames)
+#     y_pred = np.argmax(temp_data, axis=1)
+#     for x in y_pred:
+#         predictions.append(x)
+#     return predictions
 
 
 def get_frames(df, frame_size, hop_size):
